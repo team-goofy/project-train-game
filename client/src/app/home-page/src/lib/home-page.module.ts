a88b96856e76538ca609181f1b1b0d0695963a1b
Konrad <konrad-s@outlook.com>
2023-05-05 15:05:48 +0200

    chore: clean up

--- client/src/app/home-page/src/lib/home-page.module.ts
+++ client/src/app/home-page/src/lib/home-page.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { HomePageRoutingModule } from "./home-page-routing.module";
 
