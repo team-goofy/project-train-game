92e2bfec36648756fdc05ef18bf4b5dae2e3ad2f
Konrad <konrad-s@outlook.com>
2023-05-13 16:11:38 +0200

    style: remove white space

--- server/src/main/java/com/goofy/configs/FirebaseConfig.java
+++ server/src/main/java/com/goofy/configs/FirebaseConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
