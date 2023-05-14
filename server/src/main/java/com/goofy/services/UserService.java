c9a0d7d5c2353aa62fac8aba58a0bfdadcfb2b48
verhoeq <quendra.verhoef@hva.nl>
2023-05-14 18:23:04 +0200

    feat(user): Check username

--- server/src/main/java/com/goofy/services/UserService.java
+++ server/src/main/java/com/goofy/services/UserService.java
@@ -3,6 +3,8 @@ package com.goofy.services;
 import com.goofy.dtos.UserDTO;
 import com.google.firebase.auth.UserRecord;
 
+import java.util.concurrent.ExecutionException;
+
 public interface UserService {
     UserRecord saveUser(UserDTO user) throws Exception;
 
