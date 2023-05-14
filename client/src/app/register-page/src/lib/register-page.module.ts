b643a022c1e57be7cda3d0cf0f02fa194440720f
verhoeq <quendra.verhoef@hva.nl>
2023-05-14 20:58:03 +0200

    feat(user): save user

--- client/src/app/register-page/src/lib/register-page.module.ts
+++ client/src/app/register-page/src/lib/register-page.module.ts
@@ -2,14 +2,15 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RegisterPageComponent } from "./register-page/register-page.component";
 import { RegisterPageRoutingModule } from "./register-page-routing.module";
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatCardModule} from '@angular/material/card';
-import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from "@angular/material/input";
-import {ReactiveFormsModule} from "@angular/forms";
-import {BackgroundImageModule} from "@client/shared-components";
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BackgroundImageModule } from "@client/shared-components";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 @NgModule({
   imports: [
@@ -22,13 +23,14 @@ import {BackgroundImageModule} from "@client/shared-components";
     MatButtonModule,
     MatInputModule,
     ReactiveFormsModule,
-    BackgroundImageModule
+    BackgroundImageModule,
+    MatSnackBarModule
   ],
     declarations: [
       RegisterPageComponent,
     ],
   providers: [
-    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' }}
   ]
 })
 export class RegisterPageModule {}
--- client/src/app/register-page/src/lib/register-page/register-page.component.ts
+++ client/src/app/register-page/src/lib/register-page/register-page.component.ts
@@ -1,24 +1,36 @@
-import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import { Component, inject, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "@client/shared-services";
+import { UserRequestModel } from "@client/shared-models";
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent implements OnInit{
-
+  private formBuilder: FormBuilder = inject(FormBuilder);
+  private authService: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
+  private snackbar: MatSnackBar = inject(MatSnackBar);
   form: FormGroup = new FormGroup({
     email: new FormControl(''),
+    username: new FormControl(''),
     password: new FormControl('')
   });
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder) {}
-
   ngOnInit(): void {
     this.form = this.formBuilder.group(
       {
-        email: ['', [Validators.required, Validators.email]],
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}", "i"))
+          ]
+        ],
         username: [
           '',
           [
@@ -40,18 +52,37 @@ export class RegisterPageComponent implements OnInit{
   }
 
   onSubmit(): void {
-    console.log('Konrad')
     this.submitted = true;
 
     if (this.form.invalid) {
-      this.submitted=false;
       return;
     }
+
+    const user = <UserRequestModel>{
+      username: this.form.value.username,
+      email: this.form.value.email,
+      password: this.form.value.password
+    };
+
+    this.authService.register(user).subscribe({
+      next: () => {
+        let ref = this.snackbar.open(
+          "Account has been created successfully",
+          "",
+          { horizontalPosition: 'end', duration: 3000 }
+        );
+
+        ref.afterDismissed().subscribe(() => {
+          this.router.navigate(['/login']);
+        });
+      },
+      error: ({ error }) => {
+        this.snackbar.open(error.errors.join(), "Close");
+      }
+    });
   }
 
   get f(): { [key: string]: AbstractControl } {
     return this.form.controls;
   }
-
-
 }
