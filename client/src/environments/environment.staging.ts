d7535e56c88d1c2caed5dec7bc44fb90f07df8c5
verhoeq <quendra.verhoef@hva.nl>
2023-05-15 15:21:08 +0200

    ci: create staging env

---
+++ client/src/environments/environment.staging.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'train-game-6fc9f',
+    appId: '1:1001538620902:web:d6be09b15d2d7953695a27',
+    storageBucket: 'train-game-6fc9f.appspot.com',
+    apiKey: 'AIzaSyBXe_0vP-zNBO3Q0tPGYoCnVUUF2IxdCnk',
+    authDomain: 'train-game-6fc9f.firebaseapp.com',
+    messagingSenderId: '1001538620902',
+  },
+  apiUrl: 'https://train-game-staging-be.herokuapp.com/',
+  environmentName: 'staging',
+  production: false,
+};
