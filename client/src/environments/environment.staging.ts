22fa40166d9cde0c650434960fd194cc18a633c1
Konrad <konrad-s@outlook.com>
2023-05-15 17:02:46 +0200

    ci(client): add environment support

--- client/src/environments/environment.staging.ts
+++ client/src/environments/environment.staging.ts
@@ -1,4 +1,7 @@
 export const environment = {
+  environmentName: 'staging',
+  production: false,
+  apiUrl: 'https://train-game-staging-be.herokuapp.com',
   firebase: {
     projectId: 'train-game-6fc9f',
     appId: '1:1001538620902:web:d6be09b15d2d7953695a27',
@@ -6,8 +9,5 @@ export const environment = {
     apiKey: 'AIzaSyBXe_0vP-zNBO3Q0tPGYoCnVUUF2IxdCnk',
     authDomain: 'train-game-6fc9f.firebaseapp.com',
     messagingSenderId: '1001538620902',
-  },
-  apiUrl: 'https://train-game-staging-be.herokuapp.com',
-  environmentName: 'staging',
-  production: false,
+  }
 };
