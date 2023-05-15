a17ff8c2f6f603e539c0c882d1f01a3972e7b1e3
Mandy <mandy.platte@hva.nl>
2023-05-15 22:11:31 +0200

    style(login): styling for the login page desktop version

--- client/src/app/login-page/src/lib/login-page/login-page.component.ts
+++ client/src/app/login-page/src/lib/login-page/login-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "@client/shared-services";
+import { UserRequestModel } from "@client/shared-models";
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { debounceTime, distinctUntilChanged } from "rxjs";
 
 @Component({
   templateUrl: './login-page.component.html',
