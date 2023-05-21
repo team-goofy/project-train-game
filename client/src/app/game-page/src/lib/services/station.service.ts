e3479c7002c5a597c089db7df971e3c37a29c7f7
Konrad <konrad-s@outlook.com>
2023-05-21 15:37:09 +0200

    refactor(game): subject approach

--- client/src/app/game-page/src/lib/services/station.service.ts
+++ client/src/app/game-page/src/lib/services/station.service.ts
@@ -1,15 +1,24 @@
 import { HttpClient } from "@angular/common/http";
-import {Injectable, inject} from "@angular/core";
+import { Injectable, inject } from "@angular/core";
 import { Station } from "@client/shared-models";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 
 @Injectable()
 export class StationService {
     private _http: HttpClient = inject(HttpClient);
     private _baseUrl: String = environment.apiUrl;
+    private _nearestStation$ = new BehaviorSubject<Station | null>(null);
 
-    getNearestStation(lat: number, lng: number): Observable<Station> {
-        return this._http.get<Station>(`${this._baseUrl}/station/nearest`, { params: { lat, lng } });
+    init(lat: number, lng: number): void {
+        this._http
+        .get<Station>(`${this._baseUrl}/station/nearest`, { params: { lat, lng } })
+        .subscribe(station => {
+            this._nearestStation$.next(station)
+        });
+    }
+
+    get nearestStation(): Observable<Station | null> {
+        return this._nearestStation$.asObservable();
     }
 }
