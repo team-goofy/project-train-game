c928d65a8114621c6cf5570420ad3b434bbdcc6a
Mandy <mandy.platte@hva.nl>
2023-05-22 10:58:52 +0200

    fix: datetime

--- server/src/main/java/com/goofy/services/DepartureServiceImpl.java
+++ server/src/main/java/com/goofy/services/DepartureServiceImpl.java
@@ -2,12 +2,12 @@ package com.goofy.services;
 
 import com.goofy.exceptions.NoDeparturesFoundException;
 import com.goofy.exceptions.NoStationFoundException;
-import com.goofy.exceptions.UsernameExistsException;
 import com.goofy.models.Departure;
 import com.goofy.models.Departure.RouteStation;
 import com.goofy.models.ExitStationTrain;
 import org.springframework.stereotype.Service;
-import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Random;
@@ -18,20 +18,20 @@ public class DepartureServiceImpl implements DepartureService {
 
     @Override
     public List<Departure> filterByFutureDepartures(List<Departure> departures) {
-        LocalDateTime timeNow = LocalDateTime.now().plusMinutes(5);
-        LocalDateTime timeMax = timeNow.plusMinutes(30);
+        ZonedDateTime timeNow = ZonedDateTime.now(ZoneId.of("Europe/Paris")).plusMinutes(5);
+        ZonedDateTime timeMax = timeNow.plusMinutes(30);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssX");
 
         return departures.stream()
                 .filter(departure -> {
-                    LocalDateTime timestamp = LocalDateTime.parse(departure.getPlannedDateTime(), formatter);
+                    ZonedDateTime timestamp = ZonedDateTime.parse(departure.getPlannedDateTime(), formatter);
                     return timestamp.isBefore(timeMax) && timestamp.isAfter(timeNow);
                 })
                 .toList();
     }
 
     @Override
-    public ExitStationTrain getRandomExitStationTrain(List<Departure> departures) throws NoDeparturesFoundException {
+    public ExitStationTrain getRandomExitStationTrain(List<Departure> departures) throws NoDeparturesFoundException, NoStationFoundException {
         List<Departure> filteredDepartures = this.filterByFutureDepartures(departures);
 
         if(filteredDepartures.isEmpty()){
