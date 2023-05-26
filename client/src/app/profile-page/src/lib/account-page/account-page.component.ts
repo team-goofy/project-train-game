ca74ecc1a72ff77e753fb05e211513e59bdb9c46
Mandy <mandy.platte@hva.nl>
2023-05-26 18:43:11 +0200

    feat: state patern for account form

--- client/src/app/profile-page/src/lib/account-page/account-page.component.ts
+++ client/src/app/profile-page/src/lib/account-page/account-page.component.ts
@@ -1,5 +1,15 @@
 import {Component, inject} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {catchError, delay, filter, switchMap, take, tap} from "rxjs/operators";
+import {EMPTY} from "rxjs";
+
+interface State {
+  loading: boolean;
+  error: string | null;
+  editting: boolean;
+  disabled: boolean;
+}
+
 
 @Component({
   selector: 'app-account-page',
@@ -12,6 +22,61 @@ export class AccountPageComponent {
     email: new FormControl(''),
     password: new FormControl('')
   });
+
+  state: State;
+
+  constructor() {
+    this.state = this.initialState();
+  }
+
+  edittingState(): void {
+    this.state = this.initialState();
+    this.state.loading = true;
+    this.state.editting = true;
+    this.state.disabled = false;
+    console.log(this.state);
+
+    // this._permissionService.state('geolocation').pipe(
+    //   tap((state: PermissionState) => this.state.permission = state),
+    //   filter((state: PermissionState) => state === 'granted'),
+    //   switchMap(() => this._geoLocationService.pipe(take(1))),
+    //   catchError((error: GeolocationPositionError) => {
+    //     this.state = this.initialState();
+    //
+    //     if (error.code === error.PERMISSION_DENIED) {
+    //       this.state.permission = 'denied';
+    //     } else if (error.code === error.POSITION_UNAVAILABLE) {
+    //       this.state.error = 'Location information is unavailable.';
+    //     } else if (error.code === error.TIMEOUT) {
+    //       this.state.error = 'The request to get user location timed out.';
+    //     }
+    //
+    //     return EMPTY;
+    //   }),
+    //   delay(1000),
+    //   tap((position: GeolocationPosition) => {
+    //     this._stationService.init(position.coords.latitude, position.coords.longitude)
+    //     this.state.loading = false;
+    //   }),
+    //   catchError(() => {
+    //     this.state = this.initialState();
+    //     this.state.error = 'Something went wrong. Please try again later.';
+    //     return EMPTY;
+    //   })
+    // ).subscribe();
+  }
+
+
+
+  private initialState(): State {
+    return {
+      loading: false,
+      error: null,
+      editting: false,
+      disabled: true
+    };
+  }
+
 
   ngOnInit(): void {
     this.form = this.formBuilder.group(
@@ -35,8 +100,8 @@ export class AccountPageComponent {
 
 
   onSubmit(): void {
+    this.state = this.initialState();
     console.log('submit form')
-
   }
 
 }
