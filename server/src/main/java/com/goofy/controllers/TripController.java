c99b064dd5667cda92ad77d36510b0e70c2df931
verhoeq <quendra.verhoef@hva.nl>
2023-05-31 12:57:29 +0200

    feat(trip): save Trip

--- server/src/main/java/com/goofy/controllers/TripController.java
+++ server/src/main/java/com/goofy/controllers/TripController.java
@@ -11,10 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.goofy.dtos.TripImageDTO;
 import com.goofy.services.TripService;
@@ -31,6 +28,12 @@ public class TripController {
         public ResponseEntity<Object> saveTrip(@RequestBody() @Valid TripDTO trip, Principal principal) {
                 String tripId = this.tripService.saveTripToDatabase(trip, principal.getName());
                 return ResponseEntity.ok(tripId);
+        }
+
+        //TODO: getTrip
+        @GetMapping
+        public ResponseEntity<Object> getTripById(@RequestParam() String tripId) {
+                return ResponseEntity.ok().build();
         }
 
         @PostMapping(value = "/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
