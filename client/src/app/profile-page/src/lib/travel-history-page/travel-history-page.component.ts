c79a3075006c9cbc6e0fdc178bf889cb678b192f
Mandy <mandy.platte@hva.nl>
2023-06-05 12:03:55 +0200

    get all trips of the user

--- client/src/app/profile-page/src/lib/travel-history-page/travel-history-page.component.ts
+++ client/src/app/profile-page/src/lib/travel-history-page/travel-history-page.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import {MatExpansionModule} from '@angular/material/expansion';
+import {Component, inject, OnInit} from '@angular/core';
+import {AuthService, TripFilter, TripService} from "@client/shared-services";
+import {Router} from "@angular/router";
+import {Trip} from "@client/shared-models";
 
 @Component({
   selector: 'app-travel-history-page',
   templateUrl: './travel-history-page.component.html',
   styleUrls: ['./travel-history-page.component.scss']
 })
-export class TravelHistoryPageComponent {
+export class TravelHistoryPageComponent implements OnInit{
+  authService: AuthService = inject(AuthService);
+  private _tripService: TripService = inject(TripService);
 
+  private _trips: Trip[] = []
 //   iets nodig waar data van aantal trips is for forloop expanison panel
+  ngOnInit(): void {
+    const filter = <TripFilter>{ onGoing: true };
+    this._tripService.getTrips(filter).subscribe(trips => this._trips = trips);
+  }
+
+  get trips(): Trip[] {
+    return this._trips;
+  }
+
 }
